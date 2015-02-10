b5f6445614a11489a905b560a9c3a303f523562a
Richard Searle <harold.richard.searle@gmail.com>
2015-02-09 21:47:09 -0600

    first pass to SQL

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,10 @@
 
-import scala.xml.{ NodeSeq, MetaData }
-import java.io.File
-import scala.io.{ BufferedSource, Source }
+import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.apache.log4j.{ LogManager, Level }
 import org.apache.spark.storage.StorageLevel
-import org.apache.spark.SparkConf
-import java.time.LocalDateTime
-import java.time.ZoneOffset
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
 
-object StackOverflowMain extends App {
+object Main extends App {
   //LogManager.getRootLogger().setLevel(Level.WARN)
 
   val conf = new SparkConf()
@@ -33,6 +25,7 @@ object StackOverflowMain extends App {
   println("Exit")
 
   def readCommand: Boolean = {
+    import Timer._
     val command = scala.io.StdIn.readLine(">:")
     if (command == null ||
       command.isEmpty) false
@@ -64,80 +57,7 @@ object StackOverflowMain extends App {
       true
     }
   }
-  def time[T](name: String)(block: => T): T = {
-    val startTime = System.currentTimeMillis
-    val result = block // call-by-name
-    println(s"$name: ${System.currentTimeMillis - startTime}ms")
-    result
-  }
+ 
 }
 
-abstract class StackTable[T] {
 
-  val file: File
-
-  def getDate(n: scala.xml.NodeSeq): Long = n.text match {
-    case "" => 0
-    case s  => parseDate(s)
-  }
-
-  def parseDate(s: String): Long = LocalDateTime.parse(s, DateTimeFormatter.ISO_LOCAL_DATE_TIME).toInstant(ZoneOffset.UTC).getEpochSecond
-
-  def getInt(n: scala.xml.NodeSeq): Int = n.text match {
-    case "" => 0
-    case x  => x.toInt
-  }
-
-  def parseXml(x: scala.xml.Elem): T
-
-  def parse(s: String): Option[T] =
-    if (s.startsWith("  <row ")) Some(parseXml(scala.xml.XML.loadString(s)))
-    else None
-
-}
-
-object Post extends StackTable[Post] {
-
-  val file = new File("data/Posts.xml")
-  assert(file.exists)
-
-  override def parseXml(x: scala.xml.Elem): Post = Post(
-    getInt(x \ "@Id"),
-    getInt(x \ "@PostTypeId"),
-    getInt(x \ "@AcceptedAnswerId"),
-    getDate(x \ "@CreationDate"),
-    getInt(x \ "@Score"),
-    getInt(x \ "@ViewCount"),
-    (x \ "@Body").text,
-    getInt(x \ "@OwnerUserId"),
-    getDate(x \ "@LastActivityDate"),
-    (x \ "@Title").text,
-    getTags(x \ "@Tags"),
-    getInt(x \ "@AnswerCount"),
-    getInt(x \ "@CommentCount"),
-    getInt(x \ "@FavoriteCount"),
-    getDate(x \ "@CommunityOwnedDate"))
-
-  def getTags(x: scala.xml.NodeSeq): Array[String] = x.text match {
-    case "" => Array()
-    case s  => s.drop(1).dropRight(1).split("><")
-  }
-}
-
-// <row Id="1" PostTypeId="1" AcceptedAnswerId="15" CreationDate="2010-07-19T19:12:12.510" Score="19" ViewCount="1033" Body="&lt;p&gt;How should I elicit prior distributions from experts when fitting a Bayesian model?&lt;/p&gt;&#xA;" OwnerUserId="8" LastActivityDate="2010-09-15T21:08:26.077" Title="Eliciting priors from experts" Tags="&lt;bayesian&gt;&lt;prior&gt;&lt;elicitation&gt;" AnswerCount="5" CommentCount="1" FavoriteCount="11" />
-case class Post(
-  id: Int,
-  postTypeId: Int,
-  acceptedAnswerId: Int,
-  creationDate: Long,
-  score: Int,
-  viewCount: Int,
-  body: String,
-  ownerUserId: Int,
-  lastActivityDate: Long,
-  title: String,
-  tags: Array[String],
-  answerCount: Int,
-  commentCount: Int,
-  favoriteCount: Int,
-  communityOwnedDate: Long)
